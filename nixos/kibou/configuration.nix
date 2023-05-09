e7505cc80a3146d3733d6ec159967e3f1fbf20f8
Zihad <zihadmahiuddin@gmail.com>
2023-05-09 08:53:17 +0600

    kibou/packages: add git & vim

--- nixos/kibou/configuration.nix
+++ nixos/kibou/configuration.nix
@@ -84,6 +84,11 @@
 
   networking.firewall.enable = false;
 
+  environment.systemPackages = with pkgs; [
+    git
+    vim
+  ];
+
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "22.11";
 }
