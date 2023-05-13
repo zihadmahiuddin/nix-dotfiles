8c8e94367a91eb13ff88ab2de895915a77395c9f
Zihad <zihadmahiuddin@gmail.com>
2023-05-13 21:23:14 +0600

    home/user: add initial password

--- nixos/common/users.nix
+++ nixos/common/users.nix
@@ -5,6 +5,7 @@
       isNormalUser = true;
       openssh.authorizedKeys.keys = import ../../common/ssh-keys.nix;
       extraGroups = [ "wheel" "networkmanager" "audio" ];
+      initialHashedPassword = "$y$j9T$6arzU4IITk6lIwU.aiu4s.$MOJui8XwyboZ31Dm4C08kBvU0y9XVq/2fM1qXMr4VT8";
     };
   };
 }
