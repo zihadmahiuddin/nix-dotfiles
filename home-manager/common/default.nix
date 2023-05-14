d97a6f6380864727bb1157a388aa8cfe6d7ae1fd
Zihad <zihadmahiuddin@gmail.com>
2023-05-14 07:38:48 +0600

    home/tmux: init

--- home-manager/common/default.nix
+++ home-manager/common/default.nix
@@ -3,6 +3,7 @@
   imports = [
     ./git
     ./gpg
+    ./tmux
     ./zsh
   ];
 
---
+++ home-manager/common/tmux/default.nix
@@ -0,0 +1,31 @@
+{ home, pkgs, ... }:
+{
+  programs.tmux = {
+    enable = true;
+
+    mouse = true;
+    shortcut = "a";
+    baseIndex = 1;
+
+    sensibleOnTop = true;
+    plugins = with pkgs.tmuxPlugins; [
+      continuum
+      extrakto
+      fuzzback
+      jump
+      resurrect
+      tmux-thumbs
+    ];
+
+    extraConfig = ''
+      # Follow terminal background color (this is what makes transparency work)
+      set-window-option -ag window-active-style "bg=terminal"
+
+      # Fix Home and End keys not working by default
+      # https://stackoverflow.com/a/55616731
+      bind-key -n Home send Escape "OH"
+      bind-key -n End send Escape "OF"
+    '';
+  };
+}
+
