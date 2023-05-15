caeecfae96fa45ec0b3e2f10f77bf324f0b254e7
Zihad <zihadmahiuddin@gmail.com>
2023-05-15 09:07:58 +0600

    nixos/sora: use pipewire with low latency module

--- nixos/sora/misc.nix
+++ nixos/sora/misc.nix
@@ -1,5 +1,20 @@
 { ... }:
 {
   sound.enable = true;
-  hardware.pulseaudio.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+
+    lowLatency = {
+      # enable this module
+      enable = true;
+      # defaults (no need to be set unless modified)
+      quantum = 64;
+      rate = 48000;
+    };
+  };
 }
