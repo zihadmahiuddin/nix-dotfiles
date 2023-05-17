25c6431c3a1cfd69cbb841715c989a207fda4917
Zihad <zihadmahiuddin@gmail.com>
2023-05-17 10:59:25 +0600

    home/picom: init

---
+++ home-manager/desktop/picom/default.nix
@@ -0,0 +1,28 @@
+{ lib, pkgs,  ... }:
+let
+  picom = pkgs.picom-jonaburg;
+  conf = ./picom.conf;
+in
+{
+  home.packages = [ picom ];
+
+  systemd.user.services.picom = {
+    Unit = {
+      Description = "Picom X11 compositor";
+      After = [ "graphical-session-pre.target" ];
+      PartOf = [ "graphical-session.target" ];
+    };
+
+    Install = { WantedBy = [ "graphical-session.target" ]; };
+
+    Service = {
+      ExecStart = lib.concatStringsSep " " [
+        "${picom}/bin/picom"
+        "--experimental-backends"
+        "--config ${conf}"
+      ];
+      Restart = "always";
+      RestartSec = 3;
+    };
+  };
+}
