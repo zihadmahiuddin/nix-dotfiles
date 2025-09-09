cd012d21f13b61474c0a4578b9511edd67aeddc3
Zihad <zihadmahiuddin@gmail.com>
2025-09-10 00:20:27 +0600

    feat(sora): add wireguard

--- nixos/sora/newtworking.nix
+++ nixos/sora/newtworking.nix
@@ -1,7 +1,15 @@
-{ ... }:
+{ pkgs, ... }:
 {
   networking.hostName = "sora";
 
   networking.firewall.enable = false;
   networking.networkmanager.enable = true;
+
+  # Enable WireGuard kernel module
+  boot.kernelModules = ["wireguard"];
+  # Install WireGuard tools
+  environment.systemPackages = with pkgs; [
+    wireguard-tools
+    wireguard-ui
+  ];
 }
