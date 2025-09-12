a3b1acdc56c0aea3b1110243d439cffcacea0a78
Zihad <zihadmahiuddin@gmail.com>
2025-09-13 02:16:51 +0600

    fix(gaming/osu): switch to tachyon release stream
    because on windows I switched to tachyon, so I don't want conflicts

--- home-manager/desktop/gaming.nix
+++ home-manager/desktop/gaming.nix
@@ -1,6 +1,6 @@
 { inputs, pkgs, ... }: {
   home.packages = with pkgs; [
-    inputs.nix-gaming.packages.${system}.osu-lazer-bin
+    inputs.nix-gaming.packages.${system}.osu-lazer-tachyon-bin
     inputs.nix-gaming.packages.${system}.osu-stable
     winetricks
     wineWowPackages.staging
