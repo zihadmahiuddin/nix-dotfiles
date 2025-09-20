0b79761289a2510882b0055a25ea80cfcb04ae90
Zihad <zihadmahiuddin@gmail.com>
2025-09-20 15:58:56 +0600

    feat(nixos/sora): declare ethernet config w/ ipv6

--- nixos/sora/newtworking.nix
+++ nixos/sora/newtworking.nix
@@ -5,6 +5,21 @@
   networking.firewall.enable = false;
   networking.networkmanager.enable = true;
 
+  networking.networkmanager.ensureProfiles.profiles = {
+    "Ethernet" = {
+      connection = {
+        id = "ethernet";
+        interface-name = "enp34s0";
+        type = "ethernet";
+      };
+      ipv4.method = "auto";
+      ipv6.method = "auto";
+      ipv6.addresses = "2001:df5:cd40:1003::100/128";
+      ipv6.addrGenMode = "stable-privacy";
+      ipv6.neverDefault = false;
+    };
+  };
+
   # Enable WireGuard kernel module
   boot.kernelModules = ["wireguard"];
   # Install WireGuard tools
