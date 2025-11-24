5efdb9481718c655682013d65f4dd86f65994dfe
Zihad <zihadmahiuddin@gmail.com>
2025-11-25 01:24:19 +0600

    fix(nix): add myself as a trusted user

--- nixos/common/default.nix
+++ nixos/common/default.nix
@@ -45,6 +45,7 @@ in {
       # Add nix-gaming cachix
       substituters = ["https://nix-gaming.cachix.org"];
       trusted-public-keys = ["nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="];
+      trusted-users = ["zihad"];
     };
   };
 
