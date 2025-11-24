fefc1a9ac9de4daadaf7d6878f40d414f9dfd5b7
Zihad <zihadmahiuddin@gmail.com>
2025-11-25 01:10:47 +0600

    fix(discord): make krisp patcher a local file
    for some reason it was being fetched every now and then and also taking
    more than 5 minutes, just stalling for no reason even after the curl
    request finished. now all that is gone

--- pkgs/scripts/discord-krisp-patcher.nix
+++ pkgs/scripts/discord-krisp-patcher.nix
@@ -21,12 +21,7 @@
       ];
     }
     (
-      builtins.readFile (
-        pkgs.fetchurl {
-          url = "https://raw.githubusercontent.com/sersorrel/sys/afc85e6b249e5cd86a7bcf001b544019091b928c/hm/discord/krisp-patcher.py";
-          sha256 = "sha256-h8Jjd9ZQBjtO3xbnYuxUsDctGEMFUB5hzR/QOQ71j/E=";
-        }
-      )
+      builtins.readFile ./discord-krisp-patcher.py
     );
 in
   discord-krisp-patcher
