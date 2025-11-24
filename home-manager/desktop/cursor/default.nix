990aab64ae3020dd349774ea421cc836bfb25364
Zihad <zihadmahiuddin@gmail.com>
2025-11-25 01:28:19 +0600

    fix(home/cursor): fix catppuccin cursor name

--- home-manager/desktop/cursor/default.nix
+++ home-manager/desktop/cursor/default.nix
@@ -1,9 +1,8 @@
 { pkgs, ... }: {
   home.pointerCursor = {
     package = pkgs.catppuccin-cursors.mochaDark;
-    name = "Catppuccin-Mocha-Dark-Cursors";
+    name = "catppuccin-mocha-dark-cursors";
     x11.enable = true;
     gtk.enable = true;
   };
 }
-
