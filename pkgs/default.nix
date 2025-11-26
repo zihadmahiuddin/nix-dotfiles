06e86bd5a588e39a85f09d0be105d0744f4b6ae2
Zihad <zihadmahiuddin@gmail.com>
2025-11-26 21:45:15 +0600

    feat(pkgs/yt-dlp-sabr): init

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,7 +1,7 @@
 { pkgs }: {
   rebuild = pkgs.callPackage ./scripts/nix-rebuild.nix {  };
   discord-krisp-patcher = pkgs.callPackage ./scripts/discord-krisp-patcher.nix {  };
-
+  yt-dlp-sabr = pkgs.callPackage ./yt-dlp-sabr.nix {  };
   catppuccin-fcitx5 = pkgs.callPackage ./catppuccin-fcitx5.nix { };
   nvchad = pkgs.callPackage ./nvchad.nix { };
 }
