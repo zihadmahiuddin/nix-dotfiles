69fdd0acaa6057304b744b8ea4ca13310b5d3b02
Zihad <zihadmahiuddin@gmail.com>
2025-12-21 22:27:47 +0600

    fix(nixos/sora): add myself to libvirtd group

--- nixos/common/users.nix
+++ nixos/common/users.nix
@@ -4,7 +4,14 @@
     zihad = {
       isNormalUser = true;
       description = "Zihad";
-      extraGroups = [ "wheel" "networkmanager" "audio" "input" "uinput" ];
+      extraGroups = [
+        "wheel"
+        "networkmanager"
+        "audio"
+        "input"
+        "uinput"
+        "libvirtd"
+      ];
       openssh.authorizedKeys.keys = import ../../common/ssh-keys.nix;
       initialPassword = "password";
     };
