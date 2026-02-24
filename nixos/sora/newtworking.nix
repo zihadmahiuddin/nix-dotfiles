b1f68a783d6b4b31fc216341cc67125553884d7d
Zihad <zihadmahiuddin@gmail.com>
2026-02-24 18:23:19 +0600

    feat(nixos/sora): disable nm's wait-on-online

--- nixos/sora/newtworking.nix
+++ nixos/sora/newtworking.nix
@@ -6,9 +6,10 @@
 
   networking.networkmanager.enable = true;
 
+  systemd.services.NetworkManager-wait-online.enable = false;
 
   # Enable WireGuard kernel module
-  boot.kernelModules = ["wireguard"];
+  boot.kernelModules = [ "wireguard" ];
   # Install WireGuard tools
   environment.systemPackages = with pkgs; [
     wireguard-tools
