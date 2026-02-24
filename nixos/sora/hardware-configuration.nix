5b1c037b3c751fbe3bd23a3c975c4fb6a6d553ef
Zihad <zihadmahiuddin@gmail.com>
2026-02-24 18:34:41 +0600

    feat(nixos/sora): add nvidia drivers

--- nixos/sora/hardware-configuration.nix
+++ nixos/sora/hardware-configuration.nix
@@ -62,6 +62,12 @@
     };
   };
 
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+  hardware.nvidia.open = true;
+
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
