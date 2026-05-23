001595d33beec2ccb5860f63a60e8978d8ef1f86
Zihad <zihadmahiuddin@gmail.com>
2026-05-23 21:34:36 +0600

    feat(nixos): add hyprland cachix

--- nixos/common/default.nix
+++ nixos/common/default.nix
@@ -42,9 +42,10 @@ in {
       # Deduplicate and optimize nix store
       auto-optimise-store = true;
 
-      # Add nix-gaming cachix
-      substituters = ["https://nix-gaming.cachix.org"];
-      trusted-public-keys = ["nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="];
+      # Add nix-gaming and hyprland cachix
+      substituters = ["https://nix-gaming.cachix.org" "https://hyprland.cachix.org"];
+      trusted-substituters = ["https://hyprland.cachix.org"];
+      trusted-public-keys = ["nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4=" "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
       trusted-users = ["zihad"];
     };
   };
