8d3eb109b02f1db0c78e009d622c4b89e894776a
Zihad <zihadmahiuddin@gmail.com>
2026-05-23 21:34:36 +0600

    chore(nixos/sora): use hyperland's packages

--- nixos/sora/desktop.nix
+++ nixos/sora/desktop.nix
@@ -1,20 +1,26 @@
 {
   config,
+  inputs,
   lib,
   pkgs,
   ...
 }:
+let
+  hyprland-pkgs = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system};
+in
 {
   services.xserver.windowManager.i3.enable = true;
   programs.hyprland = {
     enable = true;
+    package = hyprland-pkgs.hyprland;
+    portalPackage = hyprland-pkgs.xdg-desktop-portal-hyprland;
     withUWSM = true;
     xwayland.enable = true;
   };
   xdg.portal = {
     enable = true;
     extraPortals = with pkgs; [
-      xdg-desktop-portal-hyprland
+      hyprland-pkgs.xdg-desktop-portal-hyprland
       xdg-desktop-portal-gtk
     ];
   };
