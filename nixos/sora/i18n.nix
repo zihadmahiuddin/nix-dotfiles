3cbea415347e105273e823b2cb44c0bf07731530
Zihad <zihadmahiuddin@gmail.com>
2026-05-23 21:34:36 +0600

    fix(inxos/sora): enable fcitx wayland frontend

--- nixos/sora/i18n.nix
+++ nixos/sora/i18n.nix
@@ -3,10 +3,13 @@
   i18n.inputMethod = {
     enable = true;
     type = "fcitx5";
-    fcitx5.addons = with pkgs; [
-      fcitx5-mozc
-      fcitx5-gtk
-      fcitx5-openbangla-keyboard
-    ];
+    fcitx5 = {
+      addons = with pkgs; [
+        fcitx5-mozc
+        fcitx5-gtk
+        fcitx5-openbangla-keyboard
+      ];
+      waylandFrontend = true;
+    };
   };
 }
